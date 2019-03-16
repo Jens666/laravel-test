7440c88756898ffeb3dde55b7f4e766020e329d0
richardt <olsenrichardt@gmail.com>
2019-03-17 00:03:30 +0100

    Added functionality for looking up the available date

--- app/CurlTrait.php
+++ app/CurlTrait.php
@@ -14,11 +14,11 @@ trait CurlTrait
         $ch = curl_init(); 
         curl_setopt($ch, CURLOPT_URL, $url); 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-	    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-	    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-	    curl_setopt($ch, CURLOPT_TIMEOUT, 3);
-    	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json'));
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 3);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json'));
         $output = curl_exec($ch); 
         curl_close($ch);   
 
