2ba58a3e4e6e9a64ef5925fbb9e9eae97a6f1fb8
richardt <olsenrichardt@gmail.com>
2019-03-17 00:33:46 +0100

    Added functionality for looking up the available date

--- app/Http/Controllers/UserReservationsController.php
+++ app/Http/Controllers/UserReservationsController.php
@@ -19,7 +19,7 @@ class UserReservationsController extends Controller
      */
     public function index() {
         return view('user_reservation');
-	}
+    }
 
     /**
      * Store function that returns the user_reservation view
